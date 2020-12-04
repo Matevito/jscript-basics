9c5118475ac42cedb3dc683d4bd4a880252d8fc3
Mateo <madiazt@unal.edu.co>
2020-12-04 18:28:31 -0500

    added books to the database

--- library/library.js
+++ library/library.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, n_pages, read_status){
     this.title = title
     this.author = author
@@ -7,6 +9,25 @@ function Book(title, author, n_pages, read_status){
         text_info = `${this.title} by ${this.author}, ${this.n_pages} pages, ${this.read_status}.`
         return text_info
     }
+};
+function addBookToLibrary(book){
+    myLibrary.push(book)
 }
+
+function displayBooks(library){
+    console.table(library)
+}
+
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
-console.log(hobbit.info())
+const mbovary = new Book("Madame Bovary", "G. Flaubert", 315, "read" )
+const republic = new Book("Republic", "Plato", 415, "read")
+const pEssays = new Book ("Political Essays", "D. Hume", 125, "not read yet")
+
+addBookToLibrary(hobbit)
+addBookToLibrary(mbovary)
+addBookToLibrary(republic)
+addBookToLibrary(pEssays)
+
+//console.log(myLibrary)
+//console.log(hobbit.info())
+displayBooks(myLibrary)
