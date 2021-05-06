76e6bb429b283a7045b19a38a8ef60fd29b28129
Mateo <madiazt@unal.edu.co>
2021-05-06 16:55:28 -0500

    added resetBoard function

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -1,35 +1,52 @@
 const gameBoard = (() => {
     let gameboard = ["x","x","o","x","x","x","o","x","o"]
-    // modify board function ?
-    return gameboard
+
+    const renderBoard = () => {
+        let pBoard = document.createElement("div");
+        pBoard.classList.add("board");
+        for (i = 0; i < 7; i+= 3){
+            //print the 3 lines of the board
+            let line =  document.createElement("tr");
+            for(y = 0; y < 3; y++){
+                //print the 3 cells of each liine 
+                index = i + y;
+                cell_content = gameboard[index];
+                cell = document.createElement("td");
+                cell.classList.add("boardCell")
+                cell.textContent = cell_content;
+                line.appendChild(cell);
+            }
+        pBoard.appendChild(line);}
+        let game_board = document.getElementById("gameBoard");
+        game_board.appendChild(pBoard)
+    }
+
+    const resetBoard = () => {
+        //1.reset variable
+        gameboard = [" "," "," "," "," "," "," "," "," "];
+
+        //2.remove current displayed board
+        let current_board = document.querySelector(".board");
+        let game_board = document.getElementById("gameBoard");
+        game_board.removeChild(current_board);
+
+        //3.print new board
+        renderBoard();
+    }
+
+    return {
+        renderBoard,
+        resetBoard
+    }
 })();
 const player = (number, symbol) => {
     //todo: make move function
     return number, symbol
-}
-function renderBoard(board) {
-    let pBoard = document.createElement("div");
-    pBoard.classList.add("board");
-    for (i = 0; i < 7; i+= 3){
-        //print the 3 lines of the board
-        let line =  document.createElement("tr");
-        for(y = 0; y < 3; y++){
-            //print the 3 cells of each liine 
-            index = i + y;
-            cell_content = board[index];
-            cell = document.createElement("td");
-            cell.classList.add("boardCell")
-            cell.textContent = cell_content;
-            line.appendChild(cell);
-        }
-        pBoard.appendChild(line);
-    }
-    let game_board = document.getElementById("gameBoard");
-    game_board.appendChild(pBoard)
 }
 const playGame = (() => {
     let current_board = gameBoard;
     let player_1 = player(1,"x");
     let player_2 = player(2,"0");
 })();
-renderBoard(gameBoard)
+
+gameBoard.renderBoard()
