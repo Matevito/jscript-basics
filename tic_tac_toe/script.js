87d293323ba7545dbdad0776c46741b797561712
Mateo <madiazt@unal.edu.co>
2021-05-06 19:04:25 -0500

    added check winner logic

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -52,20 +52,51 @@ const player = (number, symbol) => {
         symbol}
 }
 const playGame = (() => {
-    let current_board = gameBoard;
     let turn = 1;
-    let player_1 = player(1,"x");
-    let player_2 = player(2,"o");
-
-    //todo: play game logic
-    gameBoard.renderBoard();
+    const player_1 = player(1,"x");
+    const player_2 = player(2,"o");
     if (turn%2 == 0){
         current_player = player_2;
     }
     else{
         current_player = player_1;
     }
-    console.log(current_player)
+    //todo: logic checks winning player
+    const check_winner = () => {
+        symbol = current_player.symbol;
+        current_board = gameBoard.gameboard;
+        console.log(symbol + " i happen");
+        console.log(current_board)
+        if (current_board[0] === symbol && current_board[1] === symbol && current_board[2]){
+            return true;
+        }
+        else if(current_board[3] == symbol && current_board[4] == symbol && current_board[5] === symbol){
+            return true;
+        }
+        else if(current_board[6] == symbol && current_board[7] == symbol && current_board[8] === symbol){
+            return true;
+        }
+        else if(current_board[0] == symbol && current_board[3] == symbol && current_board[6] === symbol){
+            return true;
+        }
+        else if(current_board[1] == symbol && current_board[4] == symbol && current_board[7] === symbol){
+            return true;
+        }
+        else if(current_board[2] == symbol && current_board[5] == symbol && current_board[8] === symbol){
+            return true;
+        }
+        else if(current_board[0] == symbol && current_board[4] == symbol && current_board[8] === symbol){
+            return true;
+        }
+        else if(current_board[6] == symbol && current_board[4] == symbol && current_board[2] === symbol){
+            return true;
+        }
+        return false
+    }
+
+    //todo: play game logic
+    gameBoard.renderBoard();
+    //events logic
     cells = document.querySelectorAll(".boardCell")
     cells.forEach((cell) => {
         // add event only if the cell is free
@@ -75,6 +106,11 @@ const playGame = (() => {
             cell.textContent = current_player.symbol;
             gameBoard.refreshBoard;
             gameBoard.renderBoard;
+            turn+= 1;
+            console.log(check_winner(current_player));
         })}
     })
+    return {
+        check_winner,
+    }
 })();
