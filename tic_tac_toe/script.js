3aef9734ef945892d0186ad44d47a9e5ff8aeb47
Mateo <madiazt@unal.edu.co>
2021-05-06 21:56:40 -0500

    print and winner message

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -46,14 +46,16 @@ const gameBoard = (() => {
         gameboard,
     }
 })();
-const player = (number, symbol) => {
+const player = (name, number, symbol) => {
     return {
+        name,
         number,
         symbol}
-}
+};
 const playGame = (() => {
-    const player_1 = player(1,"x");
-    const player_2 = player(2,"o");
+    //todo:get name logic
+    const player_1 = player("mateo", 1,"x");
+    const player_2 = player("andres",2,"o");
     let turn = 1;
     const check_winner = () => {
         symbol = current_player.symbol;
@@ -90,6 +92,18 @@ const playGame = (() => {
         }
         return false
     }
+    const winner_message = (player) => {
+        message = document.createElement("div");
+        message.textContent = player.name + " is the winner!";
+        board = document.getElementById("gameBoard");
+        board.appendChild(message);
+    }
+    const tie_message = () => {
+        message = document.createElement("div");
+        message.textContent = "it's a tie!";
+        board = document.getElementById("gameBoard");
+        board.appendChild(message);
+    }
     const make_move = () => {
         if (turn%2 == 0){
             current_player = player_2;
@@ -109,23 +123,25 @@ const playGame = (() => {
                 gameBoard.gameboard[index] = current_player.symbol;
                 gameBoard.refreshBoard();
                 gameBoard.renderBoard();
+                console.log(current_player)
                 //end game logic loop
                 if (check_winner() === true){
+                    winner_message(current_player)
                     return
                 }
                 else if ( check_tie() === true){
+                    tie_message()
                     return
                 }
                 turn+= 1;
                 make_move();
         })}})
     }
-
     //todo: play game logic
     gameBoard.renderBoard();
     make_move()
     //events logic
-    return {
-        check_winner,
-    }
 })();
+const display = (()=> {
+
+})()
